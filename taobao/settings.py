02c87691932f9640046504bf4d73c3b49f1624e7
zhuhangyu <2040593@qq.com>
2015-09-03 09:35:02 +0000

    modify img folder

--- taobao/settings.py
+++ taobao/settings.py
@@ -86,7 +86,8 @@ LOG_LEVEL = 'INFO'
 DOWNLOAD_DELAY = 0.25
 COOKIES_ENABLED = True
 ITEM_PIPELINES = {'taobao.pipelines.getmmimgPipeline': 1}
-IMAGES_STORE = '/vagrant/taobao/images'
+#这是我的爬虫机器磁盘路径
+IMAGES_STORE = '/vagrant/scrapy-taobaomm/images'
 USER_AGENT_LIST = [
     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
